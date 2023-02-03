f28af5f1a250a4b394b020394e2083713eddac95
Emre Kara <emre.kara11@ogr.sakarya.edu.tr>
2023-02-03 22:20:11 +0300

    login screen input fields created, checbox module installed, a checbox added to login page, post for login added by axios but not tested

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, View} from 'react-native';
+import {Formik} from 'formik';
+import axios from 'axios';
+import Config from 'react-native-config';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
+import Input from '../components/Input';
 import Button from '../components/Button';
 
 function LogIn({navigation}) {
   //
+  const [Check, setCheck] = useState(false);
+
   function ApopToTop() {
     navigation.popToTop();
   }
+
+  function handleForm(values) {
+    axios
+      .post(Config.API_URL + 'login', {
+        tckn: values.tckn,
+        password: values.pass,
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <SafeAreaView>
-      <Text>LogIn</Text>
+      <Formik onSubmit={handleForm} initialValues={{tckn: '', pass: ''}}>
+        {({handleSubmit, handleChange, values}) => (
+          <View>
+            <Input
+              label="tckn"
+              placeholder="Write your name"
+              onChangeText={handleChange('name')}
+              value={values.tckn}
+            />
+            <Input
+              label="pass"
+              placeholder="Write your surname"
+              onChangeText={handleChange('surname')}
+              value={values.pass}
+            />
+            <Button text={'next'} onPress={handleSubmit} />
+          </View>
+        )}
+      </Formik>
       <Button text={'pop to top'} onPress={ApopToTop} />
+      <BouncyCheckbox onPress={Check} />
     </SafeAreaView>
   );
 }
